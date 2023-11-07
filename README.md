5d0a33f81234bb8685cac01635b68163241fcc23
vietlucoder <vietleruoi@gmail.com>
2023-11-07 23:16:08 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+### LAB 7
+
+#### FER201m-Front-End web development with React
+
+- Use MockApi
+- CRUD with API
+- Use Material UI
+- Login with google
+
+# 💻Tech Stack
+
+![React](https://cdn.worldvectorlogo.com/logos/react-2.svg) ![MUI](https://cdn.worldvectorlogo.com/logos/material-ui-1.svg)
---
+++ src/component/Detail/index.js
@@ -0,0 +1,54 @@
+import * as React from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { CardContent } from "@mui/material";
+import { Card, Grid } from "@mui/material";
+import { CardMedia } from "@mui/material";
+import { Typography } from "@mui/material";
+
+export default function Detail() {
+  const staff = useParams();
+
+  const [APIData, setAPIData] = useState([]);
+  const getStaffsUrl = `https://6544fc285a0b4b04436d62d4.mockapi.io/staffManagement/${staff.id}`;
+
+  useEffect(() => {
+    fetch(getStaffsUrl, { method: "GET" })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setAPIData(data);
+      })
+      .catch((error) => console.log(error.message));
+  }, [getStaffsUrl]);
+
+  return (
+    <div>
+      <h1>Detail</h1>
+      <Grid container rowSpacing={2}>
+        <Grid className="parent" item xs={12}>
+          <Card className="child" sx={{ maxWidth: 545 }}>
+            <CardMedia
+              sx={{ height: 440 }}
+              image={APIData.avatar}
+              title="green iguana"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                <div>{APIData.name}</div>
+              </Typography>
+
+              <Typography gutterBottom variant="h5" component="div">
+                {APIData.age}
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
